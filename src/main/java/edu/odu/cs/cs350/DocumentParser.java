0bd1ffab7fa7f9edeb1e87ba871719944135d543
Mariia Ellis <mvoro001@odu.edu>
2022-07-19 16:48:13 -0400

    Mariia - added placeholder for DocumentParser

--- src/main/java/edu/odu/cs/cs350/DocumentParser.java
+++ src/main/java/edu/odu/cs/cs350/DocumentParser.java
@@ -4,7 +4,6 @@
 package edu.odu.cs.cs350;
 
 /**
- * @author mariiavorobeva
  *
  */
 public class DocumentParser {
--- src/test/java/edu/odu/cs/cs350/TestDocumentParser.java
+++ src/test/java/edu/odu/cs/cs350/TestDocumentParser.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 /**
- * @author mariiavorobeva
  *
  */
 class TestDocumentParser {
