f770829c1ba95539599d3d9d99a88a73532c8a57
Mariia Ellis <mvoro001@odu.edu>
2022-07-26 15:49:21 -0400

    added code to CLI.java and TestCLI.java

--- src/main/java/edu/odu/cs/cs350/CLI.java
+++ src/main/java/edu/odu/cs/cs350/CLI.java
@@ -1,6 +1,10 @@
 package edu.odu.cs.cs350;
 
-import java.time.format.DateTimeFormatter;  
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.net.URL;
+import java.nio.file.Path;
 import java.time.LocalDateTime;  
 /*
 Each file shall be named using the data and time the processing started.
@@ -14,11 +18,96 @@ File names will take the form YYYYMMDD-hhmmss-summary.{json,txt,xlsx} where:
  */
 public class CLI {
 
-   
-     
-/*
-Collects system date and time, formats the output, and saves it to a string.
-*/  
+    private Path localPath;
+	private List<URL> siteURLs;
+    
+    /*
+     * Default Constructor
+     */
+    public CLI()
+    {
+    	localPath = null;
+    	siteURLs = new ArrayList<URL>();
+    }
+    
+    /*
+     * Non-default Constructor
+     */
+    public CLI(List<String> args)
+    {
+    	
+    }
+    
+    /*
+     * Return localPath
+     */
+    public Path getLocalPath() {
+		return localPath;
+	}
+
+    /*
+     * Set localPath
+     */
+	public void setLocalPath(Path localPath) {
+		this.localPath = localPath;
+	}
+
+	/*
+     * Return siteURLs
+     */
+	public List<URL> getSiteURLs() {
+		return siteURLs;
+	}
+	
+	/*
+	 * Returns size of siteURLs
+	 */
+	public int numberOfURLs()
+	{
+		return siteURLs.size();
+	}
+
+	/*
+     * Set siteURLs
+     */
+	public void setSiteURLs(List<URL> siteURLs) {
+		this.siteURLs = siteURLs;
+	}
+    
+    /*
+     * Takes a path to a local directory and checks if it's valid
+     * If path is valid it's assigned to localPath and return true
+     * Otherwise return false
+     */
+    public boolean isValidPath(String path)
+    {
+    	//not implemented yet
+    	return false;
+    }
+    
+    /*
+     * Takes an URL and checks if it's malformed
+     */
+    public boolean isMalformed(String testUrl)
+    {
+    	//not implemented yet
+    	return true;
+    }
+    
+    /*
+     * Takes arguments from command line and checks if URLs are valid.
+     * If URL is malformed it's discarded
+     * Otherwise it's added to siteURLs
+     */
+    public void populateSiteURLs()
+    {
+    	//not implemeted yet
+    }
+    
+    
+	/*
+	Collects system date and time, formats the output, and saves it to a string.
+	*/  
     public String outputDateTime()
     {
         DateTimeFormatter systemTime = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss-"); 
@@ -27,12 +116,13 @@ Collects system date and time, formats the output, and saves it to a string.
         return outputDateTime;
     }
   
-// Placeholder
+	// Placeholder
     public String fileSummary()
     {
         return fileSummary();
     }
-// Placeholder
+    
+	// Placeholder
     public String fileExtention()
     {
         return fileExtention();
