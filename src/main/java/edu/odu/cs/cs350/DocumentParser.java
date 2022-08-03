30a86f14734fa5805294371dd05a7268883c0478
Kendra Schall <ksscha005@odu.edu>
2022-08-03 08:34:41 -0400

    add jsoup

--- src/main/java/edu/odu/cs/cs350/DocumentParser.java
+++ src/main/java/edu/odu/cs/cs350/DocumentParser.java
@@ -4,6 +4,17 @@
 package edu.odu.cs.cs350;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.ArrayList;
+import java.util.stream.Collectors;
+import java.io.BufferedReader;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.Jsoup;
+
+
 
 /**
  *  Parse an HTML file
