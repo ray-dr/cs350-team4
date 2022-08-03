e864cbd694a0e2897d95c2a7d936e27267572918
Kendra Schall <ksscha005@odu.edu>
2022-08-03 10:36:42 -0400

    parseewith jsoup

--- src/main/java/edu/odu/cs/cs350/DocumentParser.java
+++ src/main/java/edu/odu/cs/cs350/DocumentParser.java
@@ -5,7 +5,7 @@ package edu.odu.cs.cs350;
 
 import java.nio.file.Path;
 import java.util.List;
-import java.ArrayList;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.io.BufferedReader;
 
@@ -16,12 +16,45 @@ import org.jsoup.Jsoup;
 
 
 
+
 /**
  *  Parse an HTML file
  */
 public class DocumentParser {
 	private String theTag;
 	private String theAttribute;
+
+
+
+	private DocumentParser(){
+
+	}
+
+
+	public DocumentParser(String tag, String attribute){
+		
+		this.theTag = tag;
+		this.theAttribute = attribute;
+
+		}
+
+		/*Extract tags 
+		 * @param source code 
+		 * 
+		 * @param return list of all tags 
+		 */
+
+		 public List<Element> extractAllTags(BufferedReader htmlSource){
+
+			Document doc = Jsoup.parse(htmlSource);
+			Elements elements = doc.select(this.theTag);
+
+			List<Element> elementList = new ArrayList<>();
+			for (Element elm : elements) {
+				elementList.add(elm);
+			}
+			
+		 }
 	
 	
 }
